16bf310a754a1905ae56a713e6cbe8d05640b110
Sala Matteo 2 <sala2.matteo@osdbsesto>
2022-05-10 13:29:56 +0200

    add loop

--- 3BIFirstJava/src/pkg3bifirstjava/Main.java
+++ 3BIFirstJava/src/pkg3bifirstjava/Main.java
@@ -15,7 +15,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for (int i=0;; i++){
+            System.out.println(i);
+        }
     }
     
 }
